977fb81c1c5951a67cf17cd3cbf0c863e286d6b0
asoseil <asoseil@gmail.com>
2018-02-16 19:50:25 +0100

    main tests

--- main.go
+++ main.go
@@ -4,204 +4,42 @@ import (
 	"golang.org/x/net/html"
 	"net/http"
 	"io"
-	"github.com/go-siris/siris/core/router/node"
 )
-
-// All, group of methods that return all occurrence
-type All struct {
-	*Scrappy
-}
-
-// One, group of methods that return only one occurrence
-type One struct {
-	*Scrappy
-}
 
 // Scrappy is the cool struct of the lib
 type Scrappy struct {
 	*All
-	*One
+	*First
 	deep int
-	index int
 }
 
 // New return a blank scrappy instance
-func New() Scrappy{
+func New() *Scrappy{
 	s := Scrappy{}
 	s.All = &All{&s}
-	s.One = &One{&s}
-	return s
+	s.First = &First{&s}
+	return &s
 }
 
-// Next return next sibling nodes that matches with given filters
-func (a *All) Next(root *html.Node, filters ...FilterFunc) []*html.Node {
-	var result []*html.Node
-	for node := root.NextSibling; node != nil; node = node.NextSibling {
-		if node.LastChild != nil && node.PrevSibling.Data != root.Data && node.Parent != root {
-			if a.Validate(node, filters...) != nil {
-				result = append(result, node)
-			}
+// Validate validate a node by a list of filters
+func Validate(node *html.Node, filters ...FilterFunc) bool {
+	// check node error
+	if node.Type == html.ErrorNode || len(node.Data) == 0{
+		return false
+	}
+	// loop filters
+	for _, f := range filters {
+		if f(node) {
+			continue
 		}
+		return false
 	}
-	return result
-}
-
-// Prev return prev sibling nodes that matches with given filters
-func (a *All) Prev(node *html.Node, filters ...FilterFunc) []*html.Node {
-	var result []*html.Node
-	for node := node.PrevSibling; node != nil; node = node.PrevSibling {
-		if a.Validate(node, filters...) != nil {
-			result = append(result,node)
-		}
-	}
-	return result
-}
-
-// Child return child nodes that matches with given filters
-func (a *All) Child(node *html.Node, filters ...FilterFunc) []*html.Node {
-	var result []*html.Node
-	for node := node.FirstChild; node != nil; node = node.FirstChild {
-		if a.Validate(node, filters...) != nil {
-			result = append(result,node)
-		}
-	}
-	return result
-}
-
-// Depth return nodes using first depth algorithm
-func (a *All) Depth(node *html.Node, filters ...FilterFunc) []*html.Node {
-	var result []*html.Node
-	if a.Validate(node, filters...) != nil {
-		result = append(result, node)
-	}
-	for node := node.FirstChild; node != nil; node = node.NextSibling {
-		result = append(result, a.Depth(node, filters...)...)
-	}
-	return result
-}
-
-// Parent return parent nodes that matches with given filters
-func (a *All) Parent(node *html.Node, filters ...FilterFunc) []*html.Node {
-	var result []*html.Node
-	for node := node.Parent; node != nil; node = node.Parent {
-		if a.Validate(node, filters...) != nil {
-			result = append(result, node)
-		}
-	}
-	return result
-}
-
-// Breadth return nodes using first breadth algorithm
-func (a *All) Breadth(node *html.Node, filters ...FilterFunc) []*html.Node {
-	var breadth func(nodes []*html.Node, result []*html.Node, filters ...FilterFunc) []*html.Node
-	breadth = func(nodes []*html.Node, result []*html.Node, filters ...FilterFunc) []*html.Node {
-		var next []*html.Node
-		for _, elm := range nodes {
-			for node := elm.FirstChild; node != nil; node = node.NextSibling {
-				if a.Validate(node, filters...) != nil {
-					result = append(result, node)
-				}
-				next = append(next, node)
-			}
-		}
-		if len(next) > 0 {
-			return breadth(next, result, filters...)
-		}
-		return result
-	}
-	return breadth([]*html.Node{node}, []*html.Node{}, filters...)
-}
-
-// Next return a next sibling node that matches with given filters
-func (o *One) Next(root *html.Node, filters ...FilterFunc) *html.Node {
-	for node := root.NextSibling; node != nil; node = node.NextSibling {
-		if node.LastChild != nil && node.PrevSibling.Data != root.Data && node.Parent != root {
-			if o.Validate(node, filters...) != nil {
-				return node
-			}
-		}
-	}
-	return nil
-}
-
-// Prev return a prev sibling node that matches with given filters
-func (o *One) Prev(node *html.Node, filters ...FilterFunc) *html.Node {
-	for node := node.PrevSibling; node != nil; node = node.PrevSibling {
-		if o.Validate(node, filters...) != nil {
-			return node
-		}
-	}
-	return nil
-}
-
-// Child return a child node that matches with given filters
-func (o *One) Child(node *html.Node, filters ...FilterFunc) *html.Node {
-	for node := node.FirstChild; node != nil; node = node.FirstChild {
-		if o.Validate(node, filters...) != nil {
-			return node
-		}
-	}
-	return nil
-}
-
-// Depth return a node using first depth algorithm
-func (o *One) Depth(node *html.Node, filters ...FilterFunc) *html.Node {
-	if o.Validate(node, filters...) != nil {
-		return node
-	}
-	for node := node.FirstChild; node != nil; node = node.NextSibling {
-		result := o.Depth(node, filters...)
-		if result != nil{
-			return result
-		}
-	}
-	return nil
-}
-
-// Parent return a parent node that matches with given filters
-func (o *One) Parent(node *html.Node, filters ...FilterFunc) *html.Node {
-	for node := node.Parent; node != nil; node = node.Parent {
-		if o.Validate(node, filters...) != nil {
-			return node
-		}
-	}
-	return nil
-}
-
-// Breadth return a node using first breadth algorithm
-func (o *One) Breadth(node *html.Node, filters ...FilterFunc) *html.Node {
-	var breadth func(nodes []*html.Node, filters ...FilterFunc) *html.Node
-	breadth = func(nodes []*html.Node, filters ...FilterFunc) *html.Node {
-		var next []*html.Node
-		for _, elm := range nodes {
-			for node := elm.FirstChild; node != nil; node = node.NextSibling {
-				if o.Validate(node, filters...) != nil {
-					return node
-				}
-				next = append(next, node)
-			}
-		}
-		if len(next) > 0 {
-			return breadth(next, filters...)
-		}
-		return nil
-	}
-	return breadth([]*html.Node{node}, filters...)
+	return true
 }
 
 // Deep set deep option and return a new isolated scrappy
 func (s *Scrappy) Deep(val int) *Scrappy{
-	return &Scrappy{s.All,s.One,val,0}
-}
-
-// Deep set index option and return a new isolated scrappy
-func (s *Scrappy) Index(val int) *Scrappy{
-	return &Scrappy{s.All,s.One,0,val}
-}
-
-// Mix set all the options and return a new isolated scrappy
-func (s *Scrappy) Mix(index int, deep int) *Scrappy{
-	return &Scrappy{s.All,s.One,deep,index}
+	return &Scrappy{s.All,s.First,val}
 }
 
 // Get return the content of a given url
@@ -226,23 +64,3 @@ func (s *Scrappy) Parse(reader io.Reader) (*html.Node, error){
 	return root, nil
 }
 
-// Validate validate a node by a list of filters
-func (s *Scrappy) Validate(node *html.Node, filters ...FilterFunc) *html.Node {
-	// check node error
-	if node.Type == html.ErrorNode{
-		return nil
-	}
-	// loop filters
-	for _, f := range filters {
-		if f(node) {
-			continue
-		}
-		return nil
-	}
-	// check for an index
-	if s.index != 0{
-		s.index--
-		return nil
-	}
-	return node
-}
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package scrappy
 import (
 	"testing"
 	"strings"
+	"reflect"
 	"golang.org/x/net/html"
 )
 
@@ -11,19 +12,19 @@ const (
 	document = `<!DOCTYPE html>
 	<html onload="myFunc()">
 	<head>
-		<title></title>
+		<title>title</title>
 	</head>
 	<body>
 		body text
 		<div>
-			<p>
+			<section>
 				<a href="">more</a>
 				<a href="">content</a>
-				<div>
+				<p>
 					i am also a more node
-				</div>
-			</p>
-			<p>
+				</p>
+			</section>
+			<section>
 				<ul>
 					<li>first</li>
 					<li>second</li>
@@ -31,17 +32,49 @@ const (
 					<li>forth</li>
 				</ul>
 				<p>a</p>
-			</p>
+			</section>
 		</div>
-		<i>something<i/>
-		<p>more <a href="scrappy">content</a></p>
-		<p><a href="">content</a> more onload</p>
+		<div>
+			<i>something</i>
+			<p>more <a href="scrappy">content</a></p>
+			<p><a href="">content</a> more onload</p>
+		</div>
+		<div>
+			<img src="">
+		</div>
 	</body>
 	</html>`
 )
 
+func TestNew(t *testing.T) {
+	s := New()
+	val := &Scrappy{}
+	if reflect.TypeOf(s) !=  reflect.TypeOf(val){
+		t.Fatal("Unexpected type",reflect.TypeOf(s),"instead",reflect.TypeOf(val))
+	}
+}
+
+func TestValidate(t *testing.T) {
+	node := &html.Node{Parent:nil,Type:html.ErrorNode}
+	if Validate(node){
+		t.Fatal("Unexpected, error nodes are invalid",node.Type)
+	}
+	node = &html.Node{Parent:nil,Type:html.TextNode,Data:"something"}
+	if !Validate(node){
+		t.Fatal("Unexpected, text nodes are valid",node.Type)
+	}
+	node = &html.Node{Parent:nil,Type:html.ElementNode,Data:"div"}
+	if !Validate(node,Tag("div")){
+		t.Fatal("Unexpected, div tag is present",node.Data)
+	}
+	node = &html.Node{Parent:nil,Type:html.TextNode,Data:"div"}
+	if Validate(node,Tag("div")){
+		t.Fatal("Unexpected, div tag is present but the type is wrong",node.Data,node.Type)
+	}
+}
+
 func TestScrappy_Get(t *testing.T) {
-	s := Scrappy{}
+	s := New()
 	root, err := s.Get(url)
 	if err != nil {
 		t.Fatal("Unexpected error", err)
@@ -51,191 +84,22 @@ func TestScrappy_Get(t *testing.T) {
 	}
 }
 
+func TestScrappy_Deep(t *testing.T) {
+	s := New()
+	i := 5
+	s = s.Deep(i)
+	if s.deep != i{
+		t.Fatal("Unexpected value",s.deep,"instead",i)
+	}
+
+}
+
 func TestScrappy_Parse(t *testing.T) {
-	s := Scrappy{}
+	s := New()
 	reader := strings.NewReader(document)
 	_, err := s.Parse(reader)
 	if err != nil{
 		t.Fatal("Unexpected error", err)
 	}
 
-}
-
-func TestAll_Depth(t *testing.T) {
-	s := New()
-	doc, err := html.Parse(strings.NewReader(document))
-	if err != nil {
-		t.Fatal("Unexpected error", err)
-	}
-	// test with content
-	result := s.All.Depth(doc, ContainText("more"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'more' instead", len(result))
-	}
-
-	result = s.All.Depth(doc, Text("content"))
-	if len(result) != 3 {
-		t.Fatal("Expected 3 nodes 'content' instead", len(result))
-	}
-
-	result = s.All.Depth(doc, Text("href"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'content' instead", len(result))
-	}
-
-	// test with tag
-	result = s.All.Depth(doc, Tag("a"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'a' instead", len(result))
-	}
-	result = s.All.Depth(doc, Tag("div"))
-	if len(result) != 2 {
-		t.Fatal("Expected 2 nodes 'div' instead", len(result))
-	}
-	result = s.All.Depth(doc, Tag("href"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'href' instead", len(result))
-	}
-
-	// test with attr
-	result = s.All.Depth(doc, Attr("href"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'href' instead", len(result))
-	}
-	result = s.All.Depth(doc, Attr("onload"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'onload' instead", len(result))
-	}
-	result = s.All.Depth(doc, Attr("div"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'div' instead", len(result))
-	}
-
-	// test with attr value
-	result = s.All.Depth(doc, Value("myFunc()"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'myFunc' instead", len(result))
-	}
-	result = s.All.Depth(doc, Value("scrappy"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'scrappy' instead", len(result))
-	}
-
-	// mixing
-	result = s.All.Depth(doc, Attr("onload"), Value("myFunc()"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-	result = s.All.Depth(doc, Tag("a"), Attr("href"), Value("scrappy"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-
-	// mixing with custom filter
-	custom := func(node *html.Node) bool {
-		result := s.All.Depth(node, Text("content"))
-		return len(result) > 0
-	}
-	s.All.Depth(doc, Tag("a"), Attr("href"), Value("scrappy"), custom)
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-}
-
-func TestAll_Breadth(t *testing.T) {
-	s := New()
-	doc, err := html.Parse(strings.NewReader(document))
-	if err != nil {
-		t.Fatal("Unexpected error", err)
-	}
-	// test with content
-	result := s.All.Breadth(doc, ContainText("more"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'more' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Text("content"))
-	if len(result) != 3 {
-		t.Fatal("Expected 3 nodes 'content' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Text("href"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'content' instead", len(result))
-	}
-
-	// test with tag
-	result = s.All.Breadth(doc, Tag("a"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'a' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Tag("div"))
-	if len(result) != 2 {
-		t.Fatal("Expected 2 nodes 'div' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Tag("href"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'href' instead", len(result))
-	}
-
-	// test with attr
-	result = s.All.Breadth(doc, Attr("href"))
-	if len(result) != 4 {
-		t.Fatal("Expected 4 nodes 'href' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Attr("onload"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'onload' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Attr("div"))
-	if len(result) != 0 {
-		t.Fatal("Expected 0 nodes 'div' instead", len(result))
-	}
-
-	// test with attr value
-	result = s.All.Breadth(doc, Value("myFunc()"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'myFunc' instead", len(result))
-	}
-	result = s.All.Breadth(doc, Value("scrappy"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes 'scrappy' instead", len(result))
-	}
-	// mixing
-	result = s.All.Breadth(doc, Attr("onload"), Value("myFunc()"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-	result = s.All.Breadth(doc, Tag("a"), Attr("href"), Value("scrappy"))
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-
-	// mixing with custom filter
-	custom := func(node *html.Node) bool {
-		result := s.All.Breadth(node, Text("content"))
-		return len(result) > 0
-	}
-	s.All.Breadth(doc, Tag("a"), Attr("href"), Value("scrappy"), custom)
-	if len(result) != 1 {
-		t.Fatal("Expected 1 nodes instead", len(result))
-	}
-}
-
-func TestAll_Next(t *testing.T) {
-	s := New()
-	doc, err := html.Parse(strings.NewReader(document))
-	if err != nil {
-		t.Fatal("Unexpected error", err)
-	}
-
-	node := s.One.Depth(doc, Tag("li"))
-	result := s.All.Next(node,Tag("li"))
-	if len(result) != 3 {
-		t.Fatal("Expected 3 nodes 'li' instead", len(result))
-	}
-
-	node = s.One.Depth(doc, Tag("p"))
-	result = s.All.Next(node,Tag("p"))
-	if len(result) != 2 {
-		t.Fatal("Expected 2 nodes 'p' instead", len(result))
-	}
 }
