6896d8853886be2c031464c4aa57a4d89aa21879
asoseil <asoseil@gmail.com>
2018-02-27 11:04:53 +0100

    first improved and tested

--- first.go
+++ first.go
@@ -5,40 +5,41 @@ import (
 )
 
 // First, group of methods that return only one occurrence
-type First struct {
+type F struct {
 	*Scrappy
 	index int
 }
 
-func (f *First) Index(index int) *First {
+func decrease(i int) int{
+	if i > 0 {
+		i--
+	}
+	return i
+}
+
+func (f *F) Index(index int) *F {
 	f.index = index
 	return f
 }
 
-// Return first child node that matches
-func (f *First) Child(node *html.Node, filters ...FilterFunc) *html.Node {
-	for node := node.FirstChild; node != nil; node = node.NextSibling {
-		if f.Validate(node, filters...) {
-			return node
-		}
-	}
-	return nil
-}
-
 // Return first parent node that matches
-func (f *First) Parent(root *html.Node, filters ...FilterFunc) *html.Node {
+func (f *F) Parent(root *html.Node, filters ...FilterFunc) *html.Node {
 	for node := root.Parent; node != nil; node = node.Parent {
 		if f.Validate(node, filters...) {
-			return node
+			if f.index = decrease(f.index); f.index == 0 {
+				return node
+			}
 		}
 	}
 	return nil
 }
 
 // Depth return a node using first depth algorithm, scan all nodes
-func (f *First) Depth(node *html.Node, filters ...FilterFunc) *html.Node {
+func (f *F) Depth(node *html.Node, filters ...FilterFunc) *html.Node {
 	if f.Validate(node, filters...) {
-		return node
+		if f.index = decrease(f.index); f.index == 0 {
+			return node
+		}
 	}
 	for node := node.FirstChild; node != nil; node = node.NextSibling {
 		result := f.Depth(node, filters...)
@@ -50,14 +51,16 @@ func (f *First) Depth(node *html.Node, filters ...FilterFunc) *html.Node {
 }
 
 // Breadth return a node using first breadth algorithm, scan all nodes
-func (f *First) Breadth(node *html.Node, filters ...FilterFunc) *html.Node {
+func (f *F) Breadth(node *html.Node, filters ...FilterFunc) *html.Node {
 	var breadth func(nodes []*html.Node, filters ...FilterFunc) *html.Node
 	breadth = func(nodes []*html.Node, filters ...FilterFunc) *html.Node {
 		var next []*html.Node
 		for _, elm := range nodes {
 			for node := elm.FirstChild; node != nil; node = node.NextSibling {
 				if f.Validate(node, filters...) {
-					return node
+					if f.index = decrease(f.index); f.index == 0 {
+						return node
+					}
 				}
 				next = append(next, node)
 			}
@@ -71,54 +74,73 @@ func (f *First) Breadth(node *html.Node, filters ...FilterFunc) *html.Node {
 }
 
 // Return last child, it's like a last sibling with the first child
-func (f *First) LastChild(root *html.Node, filters ...FilterFunc) *html.Node {
+func (f *F) LastChild(node *html.Node, filters ...FilterFunc) *html.Node {
+	for node := node.LastChild; node != nil; node = node.PrevSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
+				return node
+			}
+		}
+	}
+	return nil
+}
+
+// Return first child node that matches
+func (f *F) FirstChild(node *html.Node, filters ...FilterFunc) *html.Node {
+	for node := node.FirstChild; node != nil; node = node.NextSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
+				return node
+			}
+		}
+	}
 	return nil
 }
 
 // LastSibling, return last sibling node that matches
-func (f *First) LastSibling(root *html.Node, filters ...FilterFunc) *html.Node {
-	return nil
+func (f *F) LastSibling(node *html.Node, filters ...FilterFunc) (result *html.Node) {
+	for node := node.NextSibling; node != nil; node = node.NextSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
+				result = node
+			}
+		}
+	}
+	return result
 }
 
-// LastSibling, return initial sibling that matches
-func (f *First) InitSibling(root *html.Node, filters ...FilterFunc) *html.Node {
-	return nil
+// FirstSibling, return first sibling node that matches
+func (f *F) FirstSibling(node *html.Node, filters ...FilterFunc) (result *html.Node) {
+	for node := node.PrevSibling; node != nil; node = node.PrevSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
+				result = node
+			}
+		}
+	}
+	return result
 }
 
 // NextSibling, return next sibling that matches
-func (f *First) NextSibling(root *html.Node, filters ...FilterFunc) *html.Node {
-	for node := root.NextSibling; node != nil; node = node.NextSibling {
-		if len(filters) > 0 && f.Validate(node, filters...) {
-			if node.LastChild != nil && node.PrevSibling.Data != root.Data && node.Parent != root {
+func (f *F) NextSibling(node *html.Node, filters ...FilterFunc) *html.Node {
+	for node := node.NextSibling; node != nil; node = node.NextSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
 				return node
 			}
-		} else {
-			return node
 		}
 	}
 	return nil
 }
 
 // PrevSibling, return prev sibling that matches
-func (f *First) PrevSibling(root *html.Node, filters ...FilterFunc) *html.Node {
-	for node := root.PrevSibling; node != nil; node = node.PrevSibling {
-		if len(filters) > 0 && f.Validate(node, filters...) {
-			if node.LastChild != nil && node.NextSibling.Data != root.Data && node.Parent != root {
+func (f *F) PrevSibling(node *html.Node, filters ...FilterFunc) *html.Node {
+	for node := node.PrevSibling; node != nil; node = node.PrevSibling {
+		if f.Validate(node, filters...) {
+			if f.index = decrease(f.index); f.index == 0 {
 				return node
 			}
-		} else {
-			return node
 		}
 	}
-	return nil
-}
-
-// Return i nd child node that matches
-func (f *First) PositionChild(root *html.Node, i int, filters ...FilterFunc) *html.Node {
-	return nil
-}
-
-// Return i nd sibling node that matches
-func (f *First) PositionSibling(root *html.Node, i int, filters ...FilterFunc) *html.Node {
 	return nil
 }
--- first_test.go
+++ first_test.go
@@ -6,67 +6,192 @@ import (
 	"testing"
 )
 
-func TestFirst_Index(t *testing.T) {
+func TestF_Index(t *testing.T) {
 	s := New()
 	i := 5
-	f := s.First.Index(i)
+	f := s.F.Index(i)
 	if f.index != 5 {
 		t.Fatal("Unexpected error", f.index, "instead", i)
 	}
+	doc, err := html.Parse(strings.NewReader(sibling))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	node := doc.FirstChild.FirstChild.NextSibling.FirstChild.FirstChild.NextSibling
+	result := s.F.Index(2).NextSibling(node, Value("xs"))
+	if result.FirstChild.Data != "5"{
+		t.Fatal("Unexpected error, wrong node")
+	}
+
 }
 
-func TestFirst_Child(t *testing.T) {
+func TestF_Parent(t *testing.T) {
 	s := New()
 	doc, err := html.Parse(strings.NewReader(document))
 	if err != nil {
 		t.Fatal("Unexpected error", err)
 	}
 	node := doc.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling.FirstChild.NextSibling
-	result := s.First.Child(node, Tag("ul"))
+	result := s.F.Parent(node, Tag("body"))
+	if result == nil {
+		t.Fatal("Unexpected error, node missed")
+	}
+	result = s.F.Parent(node, Tag("html"), Attr("onload"))
+	if result == nil {
+		t.Fatal("Unexpected error, node missed")
+	}
+
+}
+
+func TestF_Depth(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(document))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+
+	// test with tag
+	result := s.F.Depth(doc, Tag("p"))
+	if result.FirstChild.Data != "a" {
+		t.Fatal("Unexpected error wrong node")
+	}
+
+	// test with tag, attr and value
+	result = s.F.Depth(doc, Tag("a"), Attr("href"), Value("scrappy"))
+	if result.FirstChild.Data != "content" {
+		t.Fatal("Unexpected error wrong node")
+	}
+
+	// test with a valid tag, attr and a non valid value
+	result = s.F.Depth(doc, Tag("a"), Attr("href"), Value("scrapp"))
+	if result != nil {
+		t.Fatal("Unexpected node")
+	}
+
+	// test with tag, attr and value
+	result = s.F.Depth(doc, Tag("a"), Attr("href"), ContainsValue("scrapp"))
+	if result == nil {
+		t.Fatal("Unexpected error")
+	}
+
+	// test with content
+	result = s.F.Depth(doc, Tag("a"), Attr("href"), ContainsValue("scrapp"))
+	if result == nil {
+		t.Fatal("Unexpected error")
+	}
+}
+
+func TestF_Breadth(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(document))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+
+	// test with tag
+	result := s.F.Breadth(doc, Tag("strong"))
+	if result.FirstChild.Data != "breadth" {
+		t.Fatal("Unexpected error,  wrong node")
+	}
+
+	// test with another tag
+	result = s.F.Breadth(doc, Tag("p"))
+	if !strings.Contains(result.FirstChild.Data, "more") {
+		t.Fatal("Unexpected error,  wrong node",result.FirstChild.Data)
+	}
+}
+
+func TestF_FirstChild(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(document))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	node := doc.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling.FirstChild.NextSibling
+	result := s.F.FirstChild(node, Tag("ul"))
 	if result == nil {
 		t.Fatal("Unexpected error, node missed")
 	}
 	node = doc.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling
-	result = s.First.Child(node, Tag("section"), ContainsValue("xs"), Value("col xs"))
-	if result == nil {
-		t.Fatal("Unexpected error, node missed")
+	result = s.F.FirstChild(node, Tag("section"), Value("xs"))
+	if result.Attr[0].Val != "xs" || len(result.Attr) != 1 {
+		t.Fatal("Unexpected error, wrong node")
 	}
 }
 
-func TestFirst_Parent(t *testing.T) {
+func TestF_LastChild(t *testing.T) {
 	s := New()
 	doc, err := html.Parse(strings.NewReader(document))
 	if err != nil {
 		t.Fatal("Unexpected error", err)
 	}
-	node := doc.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling.FirstChild.NextSibling
-	result := s.First.Parent(node, Tag("body"))
-	if result == nil {
-		t.Fatal("Unexpected error, node missed")
+	node := doc.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling
+	result := s.F.LastChild(node, Tag("section"), Value("xs"))
+	if result.Attr[0].Val != "xs" && len(result.Attr) != 2 {
+		t.Fatal("Unexpected error, wrong node")
 	}
-	result = s.First.Parent(node, Tag("html"), Attr("onload"))
-	if result == nil {
-		t.Fatal("Unexpected error, node missed")
-	}
-
 }
 
-func TestFirst_Depth(t *testing.T) {
+func TestF_FirstSibling(t *testing.T) {
 	s := New()
-	doc, err := html.Parse(strings.NewReader(document))
+	doc, err := html.Parse(strings.NewReader(sibling))
 	if err != nil {
 		t.Fatal("Unexpected error", err)
 	}
-	result := s.First.Depth(doc, Tag("p"))
-	if result.FirstChild.Data != "a" {
-		t.Fatal("Unexpected error wrong node")
+	node := doc.FirstChild.FirstChild.NextSibling.FirstChild.FirstChild.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling
+	result := s.F.FirstSibling(node, Value("md"))
+	if result.FirstChild.Data != "2"{
+		t.Fatal("Unexpected error, wrong node")
 	}
-	result = s.First.Depth(doc, Tag("a"), Attr("href"), Value("scrappy"))
-	if result.FirstChild.Data != "content" {
-		t.Fatal("Unexpected error wrong node")
+	result = s.F.FirstSibling(node, Value("lg"))
+	if result != nil{
+		t.Fatal("Unexpected error")
+	}
+	result = s.F.FirstSibling(node, Value("xs"))
+	if result.FirstChild.Data != "1"{
+		t.Fatal("Unexpected error, wrong node")
 	}
 }
 
-func TestFirst_Breadth(t *testing.T) {
+func TestF_LastSibling(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(sibling))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	node := doc.FirstChild.FirstChild.NextSibling.FirstChild.FirstChild.NextSibling
+	result := s.F.LastSibling(node, Value("xs"))
+	if result.FirstChild.Data != "7"{
+		t.Fatal("Unexpected error, wrong node")
+	}
+}
 
+func TestF_NextSibling(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(sibling))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	node := doc.FirstChild.FirstChild.NextSibling.FirstChild.FirstChild.NextSibling
+	result := s.F.NextSibling(node, Value("xs"))
+	if result.FirstChild.Data != "3"{
+		t.Fatal("Unexpected error, wrong node")
+	}
+}
+
+func TestF_PrevSibling(t *testing.T) {
+	s := New()
+	doc, err := html.Parse(strings.NewReader(sibling))
+	if err != nil {
+		t.Fatal("Unexpected error", err)
+	}
+	node := doc.FirstChild.FirstChild.NextSibling.FirstChild.FirstChild.NextSibling
+	result := s.F.NextSibling(node, Value("xs"))
+	if result.FirstChild.Data != "3"{
+		t.Fatal("Unexpected error, wrong node")
+	}
+	result = s.F.PrevSibling(result, Value("xs"))
+	if result.FirstChild.Data != "1"{
+		t.Fatal("Unexpected error, wrong node")
+	}
 }
